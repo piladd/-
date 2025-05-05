e7e02ec91fc6809142e838ced2ecf835e8653a65
dsavran <dsavran@sanatorium-is.ru>
2025-05-05 21:09:12 +0300

    #17офиксил авторизаю также почистил код

--- backend/Messenger.Application/Auth/DTOs/AuthResponse.cs
+++ backend/Messenger.Application/Auth/DTOs/AuthResponse.cs
@@ -12,5 +12,10 @@ public class AuthResponse
     /// <summary>
     /// Публичный ключ пользователя (SPKI, Base64)
     /// </summary>
-    public string PublicKey { get; set; } = null!;
+    public string PublicKey  { get; set; }  
+    
+    /// <summary>
+    /// Приватный ключ PKCS#8 (Base64). Возвращается только при регистрации.
+    /// </summary>
+    public string? PrivateKey { get; set; }
 }
